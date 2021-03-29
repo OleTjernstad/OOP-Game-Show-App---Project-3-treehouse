6720c72fba74ade4afa36978ede05cc2de07edaa
Ole Tjernstad <ott556@gmail.com>
2021-03-29 09:42:37 +0200

    Comments

--- js/app.js
+++ js/app.js
@@ -4,18 +4,30 @@
 
 const startGame = document.querySelector("#btn__reset");
 const keyboard = document.querySelector("#qwerty");
+/**
+ * Init the game class with phrases from phrases.js
+ */
 const game = new Game(phrases);
 
+/**
+ * listen for click on game start
+ */
 startGame.addEventListener("click", () => {
     game.startGame();
 });
 
+/**
+ * listen for click on screen keyboard
+ */
 keyboard.addEventListener("click", (event) => {
     if (event.target.tagName === "BUTTON") {
         game.handleInteraction(event.target);
     }
 });
 
+/**
+ * Listen for keydown on keyboard
+ */
 document.addEventListener("keydown", (event) =>
     game.handleInteractionFromKeyboard(event.key)
 );
